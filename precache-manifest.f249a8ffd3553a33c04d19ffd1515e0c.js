3edfe748c40b7c0e48370026f6d7a2bf56ec78b8
larenen <luki.lary@gmail.com>
2020-10-06 00:25:23 +0200

    Updates

---
+++ precache-manifest.f249a8ffd3553a33c04d19ffd1515e0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa8a696cbf58345a3600f8300037598e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "999027cee2a867d1b1dc",
+    "url": "/static/css/2.25249b3f.chunk.css"
+  },
+  {
+    "revision": "51cbded8bdfc3efed827",
+    "url": "/static/css/main.159d5fb0.chunk.css"
+  },
+  {
+    "revision": "999027cee2a867d1b1dc",
+    "url": "/static/js/2.59de764b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.59de764b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51cbded8bdfc3efed827",
+    "url": "/static/js/main.8446b0f3.chunk.js"
+  },
+  {
+    "revision": "a98948f5d970607a3297",
+    "url": "/static/js/runtime-main.c7170691.js"
+  }
+]);
